187b2281940d72d1453975e071176baac32393a3
That_Guy977 <sirawit.andy977@gmail.com>
2023-02-24 15:44:33 +0700

    Reformat urls.py

--- bioinses/urls.py
+++ bioinses/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, re_path
 from django.shortcuts import render, redirect
 
 urlpatterns = [
-  path("", lambda req: render(req, "index.html"), name="home"),
+  path("",            lambda req: render(req, "index.html"),      name="home"),
   path("simulation/", lambda req: render(req, "simulation.html"), name="simulation"),
-  path("detect/", lambda req: render(req, "detect.html"), name="detect"),
+  path("detect/",     lambda req: render(req, "detect.html"),     name="detect"),
   path("favicon.ico", lambda req: redirect("/static/logo.png")),
-  re_path(r"^.*$", lambda req: redirect("/"))
+  re_path(r"^.*$",    lambda req: redirect("/"))
 ]
