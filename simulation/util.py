8d6c9c25e86367d8d90fff9e5efeeee74de49463
That_Guy977 <sirawit.andy977@gmail.com>
2023-02-27 15:10:02 +0700

    Remove Self

--- simulation/util.py
+++ simulation/util.py
@@ -1,6 +1,6 @@
+from __future__ import annotations
 import math, random
 from pygame.math import Vector2, clamp
-from typing import Self
 
 name = "BIO-INSES"
 
@@ -11,12 +11,12 @@ tlimit = 1000
 out_dir = "simulation"
 
 class Point(Vector2):
-  def truncate(self) -> Self:
+  def truncate(self):
     self.x = float(int(self.x))
     self.y = float(int(self.y))
     return self
 
-  def clamp(self) -> Self:
+  def clamp(self):
     self.x = clamp(self.x, 0, size.x)
     self.y = clamp(self.y, 0, size.y)
     return self
@@ -28,15 +28,14 @@ class Point(Vector2):
     return f"({self.x:3.0f}, {self.y:3.0f})"
 
   @classmethod
-  def polar(cls, r: float, phi: float) -> Self:
+  def polar(cls, r: float, phi: float):
     v = cls()
     v.from_polar((r, phi))
     return v
 
   @classmethod
-  def random(cls) -> Self:
+  def random(cls):
     return cls([random.randint(0, dim) for dim in size])
 
 def deviate(m: int, s: int) -> int:
   return m + random.randint(-s, s)
-
